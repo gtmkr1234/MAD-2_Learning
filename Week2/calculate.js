71dbcb283aa45a77549c8734b0fbcb5970befbcd
gtmkr1234 <gtmkr1234@gmail.com>
2023-07-05 14:41:34 +0530

    added logging..

--- Week2/calculate.js
+++ Week2/calculate.js
@@ -1,6 +1,20 @@
-let calculate = ()=>{
+function calculate(){
     expression = document.getElementById("expression").value;
     console.log(expression);
+    try{
     answer = eval(expression);
     document.getElementById("expression").value = answer;
-};
+    addLog(expression+"="+answer);
+}
+catch(e){
+    addLog(expression+" is bad, please fix it");
+}};
+
+
+function addLog(msg){
+    var ul = document.getElementById('log');
+    var li = document.createElement('li');
+    var msg_text = document.createTextNode(msg);
+    li.appendChild(msg_text);
+    ul.appendChild(li);
+}
